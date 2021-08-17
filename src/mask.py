b05f946369c882e2aa3dfe9ce88d444a5153152d
xiangkun <946525020qq.com>
2021-08-17 13:02:30 +0800

    使用二进制掩膜法去除阴影 data->output

--- src/mask.py
+++ src/mask.py
@@ -1,135 +1,120 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import os
+from tqdm import tqdm
 
 
+def if_same_rgb(point):
+    if point[0] < 110 and point[1] < 110 and point[2] < 110:
+        return False
+    elif point[0] == point[1]+1 and point[2] == point[1]+2:
+        return True
+    elif point[0] == point[1] == point[2]:
+        return True
+    elif point[1] == point[2]-1 or point[1] == point[2]+1 or point[1] == point[2]:
+        return True
+    return False
 
-# img2 = cv2.resize(img2, (800, 800))
-# img1 = cv2.resize(img1, (800, 800))
 
-def mask_process(img1, measure):
-    img2 = img1.copy()
+def mask_process(img, measure, mask):
+    img1 = img.copy()
     # I want to put logo on top-left corner, So I create a ROI
     # 首先获取原始图像roi
-    rows, cols, channels = img2.shape
-    roi = img1[0:rows, 0:cols]
+    rows, cols, channels = img1.shape  # 获取图像2的属性
+    roi = img1[0:rows, 0:cols]  # 选择roi范围
 
-    # 原始图像转化为灰度值
-    # Now create a mask of logo and create its inverse mask also
-    img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img2gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)  # 转换为灰度图像
+    ret, mask = cv2.threshold(img2gray, 127, 255, cv2.THRESH_BINARY)  # 设置阈值，大于175的置为255，小于175的置为0
+    mask_inv = cv2.bitwise_not(mask)  # 非运算，mask取反
 
-    # cv2.imshow('img2gray', img2gray)
-    # cv2.waitKey(0)
-    '''
-    将一个灰色的图片，变成要么是白色要么就是黑色。（大于规定thresh值就是设置的最大值（常为255，也就是白色））
-    '''
-    # 将灰度值二值化，得到ROI区域掩模
-    # ret, mask = cv2.threshold(img2gray, 200, 255, cv2.THRESH_BINARY)
-    ret, mask = cv2.threshold(img2gray, 128, 255, cv2.THRESH_BINARY + measure)
-    # cv2.imshow('mask', mask)
-    # cv2.waitKey(0)
+    img1_bg = cv2.bitwise_and(roi, roi, mask=mask)  # 删除了ROI中的logo区域
+    img2_fg = cv2.bitwise_and(img1, img1, mask=mask_inv)  # 删除了logo中的空白区域
 
-    # ROI掩模区域反向掩模
-    mask_inv = cv2.bitwise_not(mask)
-
-    # cv2.imshow('mask_inv', mask_inv)
-    # cv2.waitKey(0)
-
-    # 掩模显示背景
-    # Now black-out the area of logo in ROI
-    img1_bg = cv2.bitwise_and(roi, roi, mask=mask)
-
-    # cv2.imshow('img1_bg', img1_bg)
-    # cv2.waitKey(0)
-
-    # 掩模显示前景
-    # Take only region of logo from logo image.
-    img2_fg = cv2.bitwise_and(img2, img2, mask=mask_inv)
-
-    # cv2.imshow('img2_fg', img2_fg)
-    # cv2.waitKey(0)
-
-    # 前背景图像叠加
-    # Put logo in ROI and modify the main image
-
-    mask2 = np.zeros_like(img1)
-    w = mask.shape[0]
-    h = mask.shape[1]
-    for i in range(w):
-        for j in range(h):
-            if mask[i, j] == 255:
-                mask[i, j] = 128
-    mask2[:, :, 0] = mask
-    mask2[:, :, 1] = mask
-    mask2[:, :, 2] = mask
-
-    dst = cv2.add(mask2, img2_fg)
+    dst = cv2.add(img1_bg, img2_fg)
     img1[0:rows, 0:cols] = dst
-    # img1 = cv2.fastNlMeansDenoisingColored(img1, None, 3, 3, 7, 21)
 
-    plt.subplot(2,2,1)
-    plt.imshow(mask)
 
-    plt.subplot(2,2,2)
-    plt.imshow(img1_bg)
+    # plt.subplot(2, 2, 1)
+    # plt.imshow(mask)
+    #
+    # plt.subplot(2, 2, 2)
+    # plt.imshow(img1_bg)
+    # # plt.show()
+    # plt.subplot(2, 2, 3)
+    # plt.imshow(img2_fg)
+    # # plt.show()
+    # plt.subplot(2, 2, 4)
+    # plt.imshow(dst)
     # plt.show()
-    plt.subplot(2,2,3)
-    plt.imshow(img2_fg)
+    return img2_fg
+
+
+def get_imgs(file_name):
+    project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    input_dir = os.path.join(project_dir, file_name)
+    img_dict = {}
+    for filename in os.listdir(input_dir):
+        if filename == ".DS_Store":
+            continue
+        img = cv2.imread(os.path.join(input_dir, filename))
+        img_dict[filename] = img
+    return img_dict
+
+
+def process_shadow(img):
+    final = np.zeros_like(img)
+    w = img.shape[0]
+    h = img.shape[1]
+    for i in tqdm(range(w)):
+        for j in range(h):
+            point = img[i, j, :]
+            if if_same_rgb(point):
+                final[i, j, :] = 128
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(img)
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(final)
     # plt.show()
-    plt.subplot(2,2,4)
-    plt.imshow(img1)
-    plt.show()
-    return img1
+    return final
 
 
-def log(c, img):
-    output_img = c * np.log(1.0 + img)
-    output_img = np.uint8(output_img + 0.5)
-    return output_img
+def process_bg(img):
+    w = img.shape[0]
+    h = img.shape[1]
+    for i in tqdm(range(w)):
+        for j in range(h):
+            point = img[i, j, :]
+            if point[0] == point[1] == point[2] == 0:
+                img[i, j, :] = 128
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(img)
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(final)
+    # plt.show()
+    return img
 
+if __name__ == '__main__':
+    img_dict = get_imgs("data")
+    for img_name, first_img in img_dict.items():
 
-img_name = "12.jpg"
-img = cv2.imread(f"../lab_test/{img_name}")
-blur = cv2.GaussianBlur(img, (5, 5), 0)
-# from img_matting import get_hist
-# get_hist(blur)
-# w = blur.shape[0]
-# h = blur.shape[1]
-# for i in range(w):
-#     for j in range(h):
-#         if (int(blur[i, j, 0]) + int(blur[i, j, 1]) + int(blur[i, j, 2])) // 3 >= 225:
-#             blur[i, j, :] = 225
+        second_img = process_shadow(first_img)
+        # final = mask_process(img, cv2.THRESH_OTSU)
+        Ksize = 9  # 核函数尺寸，取值范围3-27之间的奇数，默认为3；建议取值3、5、7过大图像模糊化严重，运算慢
+        third_img = cv2.medianBlur(second_img, ksize=Ksize)
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(img)
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(out)
+        # plt.show()
+        forth_img = mask_process(first_img, cv2.THRESH_TRIANGLE, third_img)
+        out = process_bg(forth_img)
+        cv2.imwrite(f"../output/{img_name}", out)
+        print(f"{img_name} written successfully!")
+    # img_dict = get_imgs("mask")
+    # for img_name, img in img_dict.items():w
+    #     dst = cv2.fastNlMeansDenoisingColored(img, None, 15, 10, 7, 21)
+    #     plt.subplot(121), plt.imshow(img)
+    #     plt.subplot(122), plt.imshow(dst)
+    #     plt.show()
 
-# kernel = np.array([[0, -1, 0],
-#                    [-1, 5, -1],
-#                    [0, -1, 0]])
-
-# dst = cv2.filter2D(img, -1, kernel)
-
-from lab import shadow_remove
-# blur = shadow_remove(blur)
-# blur = cv2.GaussianBlur(img, (3, 3), 0)
-# blur = cv2.blur(img,(2,2))
-# b,g,r = cv2.split(blur)
-# clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(4, 4))
-# b = clahe.apply(b)
-# g = clahe.apply(g)
-# r = clahe.apply(r)
-# blur = cv2.merge([b,g,r])
-# b,g,r = cv2.split(img)
-
-# b = cv2.equalizeHist(b)
-# g = cv2.equalizeHist(g)
-# r = cv2.equalizeHist(r)
-# blur = cv2.merge([b,g,r])
-
-plt.figure(figsize=(12, 8))
-plt.subplot(1, 2, 1)
-plt.imshow(img)
-plt.subplot(1, 2, 2)
-plt.imshow(blur)
-plt.show()
-# final = mask_process(blur, cv2.THRESH_TRIANGLE)
-final = mask_process(blur, cv2.THRESH_TRIANGLE)
-# final = mask_process(final, cv2.THRESH_TRIANGLE)
-cv2.imwrite(f"../lab_mask/{img_name}", final)
