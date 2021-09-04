cf9bf14378ce1615f94e50cb81e31402f9d95bdd
xiangkun <946525020qq.com>
2021-09-04 23:23:58 +0800

    代码修正

--- src/get_bgr.py
+++ src/get_bgr.py
@@ -1,7 +1,7 @@
 import cv2
-import numpy as np
+
 # 读取图片并缩放方便显示
-img = cv2.imread('../data/15.jpg')
+img = cv2.imread('../data/4.jpg')
 height, width = img.shape[:2]
 size = (int(width * 0.2), int(height * 0.2))
 # 缩放
@@ -17,22 +17,13 @@ def getposHsv(event, x, y, flags, param):
         print("HSV is", HSV[y, x])
 
 
-
 def getposBgr(event, x, y, flags, param):
-    w = img.shape[0]
-    h = img.shape[1]
-    mask = np.zeros((w + 2, h + 2), np.uint8)
     if event == cv2.EVENT_LBUTTONDOWN:
         print("Bgr is", img[y, x])
-        # cv2.floodFill(img, mask, (y, x), (255, 255, 255), (2, 2, 3), (4, 4, 3), 4)
-        # cv2.imshow("floodfill", img)
-        # cv2.waitKey(0)
-        # cv2.destroyWindow("floodfill")
 
 
 cv2.imshow("imageHSV", HSV)
 cv2.imshow('image', img)
 cv2.setMouseCallback("imageHSV", getposHsv)
 cv2.setMouseCallback("image", getposBgr)
-
 cv2.waitKey(0)
--- src/mask.py
+++ src/mask.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 from tqdm import tqdm
 
 
 def if_same_rgb(point):
+    # 判定rgb值是否相似，true表示检测到的像素为图像阴影或背景
     if point[0] < 110 and point[1] < 110 and point[2] < 110:
         return False
     elif point[0] == point[1]+1 and point[2] == point[1]+2:
@@ -19,19 +19,12 @@ def if_same_rgb(point):
 
 def process_mask(img, mask):
     img1 = img.copy()
-    # I want to put logo on top-left corner, So I create a ROI
-    # 首先获取原始图像roi
-    rows, cols, channels = img1.shape  # 获取图像2的属性
-    roi = img1[0:rows, 0:cols]  # 选择roi范围
-
-    img2gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)  # 转换为灰度图像
-    ret, mask = cv2.threshold(img2gray, 127, 255, cv2.THRESH_BINARY)  # 设置阈值，大于175的置为255，小于175的置为0
+    # 首先获取原始图像
+    img_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)  # 转换为灰度图像
+    ret, mask = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)  # 设置阈值，大于175的置为255，小于175的置为0
     mask_inv = cv2.bitwise_not(mask)  # 非运算，mask取反
-
-    img1_bg = cv2.bitwise_and(roi, roi, mask=mask)  # 删除了ROI中的前景区域
-    img2_fg = cv2.bitwise_and(img1, img1, mask=mask_inv)  # 删除了背景区域
-
-    return img2_fg
+    img_fg = cv2.bitwise_and(img1, img1, mask=mask_inv)  # 删除了背景区域
+    return img_fg
 
 
 def get_imgs(file_name):
@@ -51,7 +44,7 @@ def get_imgs(file_name):
 
 def process_shadow(img):
     # 检测阴影，并将其和背景一起置为灰色
-    final = np.zeros_like(img)
+    mask = np.zeros_like(img)
     w = img.shape[0]
     h = img.shape[1]
     for i in tqdm(range(w), desc="Processing Shadow:"):
@@ -60,8 +53,8 @@ def process_shadow(img):
             # 若该点的rgb值相同或近似相同，则判断为阴影
             if if_same_rgb(point):
                 # 阴影全部置为灰色
-                final[i, j, :] = 128
-    return final
+                mask[i, j, :] = 128
+    return mask
 
 
 def process_bg(input_img):
@@ -75,33 +68,3 @@ def process_bg(input_img):
             if point[0] == point[1] == point[2] == 0:
                 img[i, j, :] = 128
     return img
-
-
-if __name__ == '__main__':
-    img_dict = get_imgs("data")
-    for img_name, first_img in img_dict.items():
-        # 去除阴影，生成掩膜
-        second_img = process_shadow(first_img)
-        # 消去白色噪点
-        # 核函数尺寸，取值范围3-27之间的奇数，默认为3；建议取值3、5、7过大图像模糊化严重，运算慢
-        k_size = 9
-        third_img = cv2.medianBlur(second_img, ksize=k_size)
-        # 根据掩膜过滤背景
-        forth_img = process_mask(first_img, third_img)
-        # 将黑色背景转为灰色
-        out = process_bg(forth_img)
-
-        plt.subplot(2, 2, 1)
-        plt.imshow(second_img)
-        plt.subplot(2, 2, 2)
-        plt.imshow(third_img)
-        plt.subplot(2, 2, 3)
-        plt.imshow(forth_img)
-        plt.subplot(2, 2, 4)
-        plt.imshow(out)
-        plt.show()
-
-        cv2.imwrite(f"../output/{img_name}", out)
-        print(f"{img_name} written successfully!")
-
-
